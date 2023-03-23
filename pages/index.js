5dff2e7735c2e43734ebf8d6778a96d59a43c551
Christian Duarte <cduarte3@myseneca.ca>
2023-03-23 10:13:11 -0400

    final push

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 /*********************************************************************************
-*  BTI425 – Assignment 4
+*  BTI425 – Assignment 5
 *  I declare that this assignment is my own work in accordance with Seneca Academic Policy.  
 *  No part of this assignment has been copied manually or electronically from any other source
 *  (including web sites) or distributed to other students.
 * 
-*  Name: Christian Duarte Student ID: 158217208 Date: March 9th 2023
+*  Name: Christian Duarte Student ID: 158217208 Date: March 18th 2023
 *
 ********************************************************************************/
 import Row from 'react-bootstrap/Row';
