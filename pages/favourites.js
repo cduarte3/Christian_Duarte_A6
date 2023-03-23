c569e24fc2482d5090c631cdc2ae564179839577
Christian Duarte <cduarte3@myseneca.ca>
2023-03-23 10:10:30 -0400

    final

--- pages/favourites.js
+++ pages/favourites.js
@@ -9,12 +9,13 @@ export default function Favourites() {
 
     const [favouritesList] = useAtom(favouritesAtom);
     if (favouritesList != null || favouritesList != undefined) {
-        return (<>
-            <Row className="gy-4">
-                {favouritesList.length > 0 ? favouritesList.map((currentObjectID) => (
-                    <Col lg={3} key={currentObjectID}><ArtworkCard objectID={currentObjectID} /></Col>
-                )) : <Card className='text-center'><br /><h4>Nothing Here</h4><p> Try adding some Artwork to favourites</p><br /></Card>}
-            </Row>
-        </>)
+        return (
+            <>
+                <Row className="gy-4">
+                    {favouritesList.length > 0 ? favouritesList.map((currentObjectID) => (
+                        <Col lg={3} key={currentObjectID}><ArtworkCard objectID={currentObjectID} /></Col>
+                    )) : <Card className='text-center'><br /><h4>Nothing Here</h4><p> Try adding some Artwork to favourites</p><br /></Card>}
+                </Row>
+            </>)
     }
 }
